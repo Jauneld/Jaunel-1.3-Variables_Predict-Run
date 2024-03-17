e65cdfc9ccfe7395b72da13f8da0ad9f078ed9b4
Jdeans@2025 <144694485+Jauneld@users.noreply.github.com>
2024-03-17 01:06:05 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#Jaunel Deans
+#September 18, 2023
+#We are predicting what will happen in the code. 
+
+#Predict and Run Example 1
+ # The code will print what is in the variabel firstName which is Andy. 
+
+firstName = "Andy"
+
+print(firstName)
+
+#Predict and Run Example 2
+ # The code will print what is in the variable fullName. Within full name, there is the 
+#concatenation of the two variables firstName, lastName and a space. Therefore the code 
+# will print Andy Colley
+
+lastName = "Colley"
+
+fullName = firstName + " " + lastName
+
+print(fullName)
+
+#Predict and Run Example 3
+ # The code will print "Hello Andy. Your full name is Andy Colley."
+
+print("Hello " + firstName + ". Your full name is " + fullName + ".")
+
